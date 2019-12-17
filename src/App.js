05b412cdf81614eed5f69e492b1c0be38e91ae7c
Vladyslav <terhoha@gmail.com>
2019-12-17 11:35:43 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import { Auth } from './pages/pages'
 
 class App extends Component {
   render(){
     return (
       <div className='wrapper'>
-        <Auth />
+        hello world
       </div>
     )
   }
