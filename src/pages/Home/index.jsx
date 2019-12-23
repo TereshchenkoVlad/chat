96c76fb582f9768b1829bc87f2f0540f5efffd98
Vladyslav Tereshchenko <terhoha@gmail.com>
2019-12-23 17:14:31 +0200

    new changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,14 @@ const Home = () => {
         <section>
             <Message 
                 avatar='https://image.flaticon.com/icons/png/512/186/premium/186313.png' 
-                text='Привіт друже як ти? Вже став міддл розробником?😂' date={new Date('2019-12-12')} />
+                text='Привіт друже як ти? Вже став міддл розробником?😂' date={new Date('2019-12-12')} 
+            />
+            <Message 
+                avatar='https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png' 
+                text='Я вже  Senior, looser! Поняв да?!Я вже  Senior, looser! Поняв да?!' date={new Date('2019-12-20')} 
+                isMe={true}
+                isReaded={true}
+            />
         </section>
     )
 }
